f8afd0d18a53e9786334e6224d634399d25e3da4
Verano <verano@verano.ve>
2022-07-11 09:33:16 +0300

    Backup changed

--- quasar.config.js
+++ quasar.config.js
@@ -83,7 +83,7 @@ module.exports = configure(function (ctx) {
       proxy: {
         // proxy all requests starting with /api to jsonplaceholder
         "/api": {
-          target: "http://127.0.0.1:8000",
+          target: "http://activity-monitoring.nadsvebm.beget.tech",
           changeOrigin: true,
           pathRewrite: {
             "^/api": "",
